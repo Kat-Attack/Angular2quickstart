0ea8ba2668a1a7ad393d24c1769e27857a8c3370
Kathy Xu <kxu08@hotmail.com>
2016-09-19 16:23:34 -0400

    tour of heros step 2 finished

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,23 @@
 import { NgModule }      from '@angular/core';
+
+//imports browser module since this is a web application that runs in a browser
 import { BrowserModule } from '@angular/platform-browser';
+
+//imports module to help with forms and two way binding (ngModel)
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [ 
+  	BrowserModule,
+  	FormsModule 
+  ],
+  declarations: [ 
+  	AppComponent 
+  ],
+  bootstrap: [
+  	AppComponent
+  ]
 })
 export class AppModule { }
